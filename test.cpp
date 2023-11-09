427b7aaa74bc1eeac8592c2369de33be080f51fb
Nguyễn Hữu Niên <huunien734@gmail.com>
2023-11-09 20:23:04 +0700

    update file test

--- test.cpp
+++ test.cpp
@@ -1,60 +1,31 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#include<sstream>
 
 using namespace std;
 
-int main() {
-	int m, n;
-	// nhap m va n
-	cin >> m >> n;
-	int arr[m][n];
-	for (int i = 0;i < m;i++) {
-		for (int j = 0;j < n;j++) {
-			cin >> arr[i][j];
+vector<int> gradingStudents(vector<int> grades) {
+	vector<int> diem;
+	for(auto o : grades){
+		if(o<38){
+			diem.push_back(o);
+		}else{
+			int x = ((o / 10) * 10) + 5;
+			if(x < o) x += 5;
+			if(x - o < 3) diem.push_back(x);
+			else diem.push_back(o);
 		}
 	}
+	return diem;
+}
 
-	// tinh tong cac phan tu cua ma tran
-	int sum = 0;
-	for (int i = 0;i < m;i++) {
-		for (int j = 0;j < n;j++) {
-			sum += arr[i][j];
-		}
+int main(){
+	int n, a;
+	vector<int> vt;
+	cin >> n ;
+	for(int i=0;i<n;i++){
+		cin >> a;
+		vt.push_back(a);
 	}
-	cout << sum << endl;
-	
-	// tinh tong cac phan tu am cua ma tran
-	int sumAm = 0;
-	for (int i = 0;i < m;i++) {
-		for (int j = 0;j < n;j++) {
-			if (arr[i][j] < 0) {
-				sumAm += arr[i][j];
-			}
-		}
-	}
-	if (sumAm == 0) cout << "Khong co phan tu am";
-	else cout << sumAm << endl;
-
-	// tim gt lon nhat va nho nhat tren duong cheo
-	int xMax, xMin;
-	int maxArr = arr[0][0], minArr = arr[0][0];
-	if (m != n) {
-		cout << "Ma tran khong co duong cheo" << endl;
-		return 0;
-	}
-	else {
-		for (int i = 0;i < m;i++) {
-			if (arr[i][i] < minArr) {
-				minArr = arr[i][i];
-				xMin = i;
-			}
-			else if (arr[i][i] > maxArr) {
-				maxArr = arr[i][i];
-				xMax = i;
-			}
-		}
-	}
-	cout << "Max: " << maxArr << " - vi tri: " << xMax << ":" << xMax << endl;
-	cout << "Min: " << minArr << " - vi tri: " << xMin << ":" << xMin << endl;
-
+	for(auto o : gradingStudents(vt)) cout << o << endl;
 	return 0;
 }
