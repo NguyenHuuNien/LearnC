9e44e0d1a5f56334866bc5172789f3faf90a4b73
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-11 00:13:20 +0700

    test update

--- test.cpp
+++ test.cpp
@@ -1,31 +1,17 @@
 #include<bits/stdc++.h>
-#include<sstream>
 
 using namespace std;
 
-vector<int> gradingStudents(vector<int> grades) {
-	vector<int> diem;
-	for(auto o : grades){
-		if(o<38){
-			diem.push_back(o);
-		}else{
-			int x = ((o / 10) * 10) + 5;
-			if(x < o) x += 5;
-			if(x - o < 3) diem.push_back(x);
-			else diem.push_back(o);
-		}
-	}
-	return diem;
-}
-
 int main(){
-	int n, a;
-	vector<int> vt;
-	cin >> n ;
-	for(int i=0;i<n;i++){
-		cin >> a;
-		vt.push_back(a);
+	int n = 50;
+	while(n>0){
+
+		cout << "n = " << n << " | " << "n % 9 = " << n%9 << " | " << endl;
+		if(n%9!=0)
+			n -= n%9;
+		else
+			n--;
 	}
-	for(auto o : gradingStudents(vt)) cout << o << endl;
+
 	return 0;
 }
